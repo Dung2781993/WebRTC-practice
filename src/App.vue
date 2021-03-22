d5c3701fa090dc8c2fb6dfbb910118b6c58df183
NCLAN\lndu <Ngoc Dung Le>
2021-03-22 11:31:29 +0700

    Check attendence

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       @logout="logout"
       @deleteRoom="deleteRoom"
       @addRoom="addRoom"
+      @checkIn="checkIn"
     />
   </div>
 </template>
@@ -47,6 +48,26 @@ export default {
         .collection('rooms')
         .doc(payload)
         .delete()
+    },
+    checkIn: function(payload) {
+      const roomRef = db
+        .collection('users')
+        .doc(payload.hostID)
+        .collection('rooms')
+        .doc(payload.roomID)
+
+      roomRef.get().then(doc => {
+        if (doc.exists) {
+          roomRef
+            .collection('attendees')
+            .doc(this.user.uid)
+            .set({
+              displayName: payload.displayName,
+              createdAt: Firebase.firestore.FieldValue.serverTimestamp()
+            })
+            .then(() => this.$router.push(`/chat/${payload.hostID}/${payload.roomID}`))
+        }
+      })
     }
   },
   mounted() {
